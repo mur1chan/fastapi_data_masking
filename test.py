f29e7b9ef47ee29febfb9e84a984011dc5ad471c
AlexW02 <70980304+AlexW02@users.noreply.github.com>
2024-01-11 19:33:17 +0100

    token fix

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from fastapi.testclient import TestClient
 from app.main import app
 
 client = TestClient(app)
+token = None
 
 def test_login_for_access_token():
     response = client.post("/token", data={
@@ -18,6 +19,7 @@ def test_login_for_access_token():
     assert "access_token" in response.json()
 
 def test_anonymize():
+    global token
     headers = {'Authorization': 'Bearer '+token}
     test_data = {"values": ["test1", "test2"], "password": "EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW"}
     response = client.post("/anonymize", headers=headers, json=test_data)
@@ -27,6 +29,7 @@ def test_anonymize():
     assert len(data["values"]) == len(test_data["values"])
 
 def test_pseudonymize():
+    global token
     headers = {'Authorization': 'Bearer '+token}
     test_data = {"values": ["test1", "test2"], "password": "EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW"}
     response = client.post("/pseudonymize", headers=headers, json=test_data)
@@ -36,6 +39,7 @@ def test_pseudonymize():
     assert len(data["values"]) == len(test_data["values"])
 
 def test_unpseudonymize():
+    global token
     headers = {'Authorization': 'Bearer '+token}
     test_data = {"values": ["0301f20878cb68a850a9b5f041a605e55281ff1fb209ded875d3f0544397bb02f0388b03e6ca20203baec6c5cc9c23f0b667d5f6ddbc5bce75d7a64e99aaeb916992cc141d03b9e3351df3cfac612a4be966"], "password": "EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW"}
     response = client.post("/unpseudonymize", headers=headers, json=test_data)
